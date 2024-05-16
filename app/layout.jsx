4e5c5f31790f279b773a6bbe188354819ed02dc5
Ali Ahmed <alitine94@gmail.com>
2024-05-16 03:35:48 -0400

    next auth configured

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import HomePage from "./page";
 import PropertyPage from "./properties/page";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "Property Pulse | Find The Perfect Rental",
@@ -12,13 +13,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
