338da765317990795c245752abf4d66399ac0fa1
Ali Ahmed <alitine94@gmail.com>
2024-05-20 22:51:05 -0400

    add property form

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -17,3 +17,44 @@ export const GET = async (request) => {
     });
   }
 };
+
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+    const amenities = formData.getAll("amenities");
+    const images = formData
+      .getAll("images")
+      .filter((image) => image.name !== "");
+
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        zipcode: formData.get("location.zipcode"),
+        state: formData.get("location.state"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square-feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        nightly: formData.get("rates.nightly"),
+        monthly: formData.get("rates.monthly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      images,
+    };
+
+    return new Response(JSON.stringify({ message: "success" }), {
+      status: 200,
+    });
+  } catch (error) {}
+};
