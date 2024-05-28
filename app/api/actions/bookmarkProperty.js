958f120d608a414604e1da7ab61d6bba77ac8d75
Ali Ahmed <alitine94@gmail.com>
2024-05-28 01:07:28 -0400

    check bookmark status

--- app/api/actions/bookmarkProperty.js
+++ app/api/actions/bookmarkProperty.js
@@ -1,37 +1,41 @@
+"use server";
+
 import connectDB from "@/config/db";
-import Property from "@/models/Property";
 import User from "@/models/User";
 import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
 
-export const dynamic = "force-dynamic";
+const bookmarkProperty = async (propertyId) => {
+  await connectDB();
 
-export const POST = async () => {
   try {
     await connectDB();
 
-    const propertyId = await request.json();
+    const { propertyId } = await request.json();
 
     const sessionUser = await getSessionUser();
 
     if (!sessionUser || !sessionUser.userId) {
-      return new Response("User ID is required", {
-        status: 401,
-      });
+      return new Response("User ID is required", { status: 401 });
     }
 
     const { userId } = sessionUser;
 
+    // Find user in database
     const user = await User.findOne({ _id: userId });
 
+    // Check if property is bookmarked
     let isBookmarked = user.bookmarks.includes(propertyId);
 
     let message;
 
     if (isBookmarked) {
+      // If already bookmarked, remove it
       user.bookmarks.pull(propertyId);
       message = "Bookmark removed successfully";
       isBookmarked = false;
     } else {
+      // If not bookmarked, add it
       user.bookmarks.push(propertyId);
       message = "Bookmark added successfully";
       isBookmarked = true;
@@ -39,11 +43,10 @@ export const POST = async () => {
 
     await user.save();
 
-    return new Response(JSON.stringify({ message, isBookmarked }), {
-      status: 200,
-    });
+    return { message, isBookmarked };
   } catch (error) {
     console.log(error);
-    return new Response("Something went wrong", { status: 500 });
   }
 };
+
+export default bookmarkProperty;
--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import bookmarkProperty from "@/app/api/actions/bookmarkProperty";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import { FaBookmark } from "react-icons/fa";
@@ -17,29 +18,22 @@ const BookmarkButton = ({ property }) => {
       return;
     }
 
-    try {
-      const res = await fetch("/api/bookmarks", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          propertyId: property._id,
-        }),
-      });
-
-      if (res.status === 200) {
-        const data = await res.json();
-        toast.success(data.message);
-        setIsBookmarked(data.isBookmarked);
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error("Something went wrong");
-    }
+    bookmarkProperty(property._id).then((res) => {
+      if (res.error) return toast.error(res.error);
+      setIsBookmarked(res.isBookmarked);
+      toast.success(res.message);
+    });
   };
 
-  return (
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" />
+      Remove bookmark
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
