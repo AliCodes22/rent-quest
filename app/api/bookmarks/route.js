d3e02c0686eea6c33444360c2595c2f5bf44ae1d
Ali Ahmed <alitine94@gmail.com>
2024-05-29 00:59:40 -0400

    debugging

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -5,33 +5,35 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
 
-export const POST = async () => {
+export const POST = async (request) => {
   try {
     await connectDB();
 
-    const propertyId = await request.json();
+    const { propertyId } = await request.json();
 
     const sessionUser = await getSessionUser();
 
     if (!sessionUser || !sessionUser.userId) {
-      return new Response("User ID is required", {
-        status: 401,
-      });
+      return new Response("User ID is required", { status: 401 });
     }
 
     const { userId } = sessionUser;
 
+    // Find user in database
     const user = await User.findOne({ _id: userId });
 
+    // Check if property is bookmarked
     let isBookmarked = user.bookmarks.includes(propertyId);
 
     let message;
 
     if (isBookmarked) {
+      // If already bookmarked, remove it
       user.bookmarks.pull(propertyId);
       message = "Bookmark removed successfully";
       isBookmarked = false;
     } else {
+      // If not bookmarked, add it
       user.bookmarks.push(propertyId);
       message = "Bookmark added successfully";
       isBookmarked = true;
--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,10 +1,8 @@
 "use client";
-
-import bookmarkProperty from "@/app/api/actions/bookmarkProperty";
-import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
-import { FaBookmark } from "react-icons/fa";
+import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
+import { FaBookmark } from "react-icons/fa";
 
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
@@ -14,34 +12,39 @@ const BookmarkButton = ({ property }) => {
 
   const handleClick = async () => {
     if (!userId) {
-      toast.error("You must be logged in");
+      toast.error("Must be signed in");
       return;
     }
 
-    bookmarkProperty(property._id).then((res) => {
-      if (res.error) return toast.error(res.error);
-      setIsBookmarked(res.isBookmarked);
-      toast.success(res.message);
-    });
+    try {
+      const res = await fetch("/api/bookmarks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          propertyId: property._id,
+        }),
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        setIsBookmarked(data.isBookmarked);
+        toast.success(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
   };
 
-  return isBookmarked ? (
-    <button
-      onClick={handleClick}
-      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-    >
-      <FaBookmark className="mr-2" />
-      Remove bookmark
-    </button>
-  ) : (
+  return (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
     >
-      <FaBookmark className="mr-2" />
-      Bookmark Property
+      <FaBookmark className="mr-2" /> Bookmark Property
     </button>
   );
 };
-
 export default BookmarkButton;
