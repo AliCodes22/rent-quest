3e409079a734cf157f4b01dd852631790431505f
Ali Ahmed <alitine94@gmail.com>
2024-06-01 05:10:17 -0400

    share buttons, check bookmark hook

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -38,7 +38,41 @@ const BookmarkButton = ({ property }) => {
     }
   };
 
-  return (
+  useEffect(() => {
+    const checkBookmark = async () => {
+      try {
+        const res = await fetch("/api/bookmarks/check", {
+          method: "POST",
+          body: JSON.stringify({
+            propertyId: property._id,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setIsBookmarked(data.isBookmarked);
+        } else {
+          throw new Error("Bookmark check failed");
+        }
+      } catch (error) {
+        console.error("Error checking bookmark:", error);
+      }
+    };
+
+    checkBookmark();
+  }, [property._id]);
+
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
