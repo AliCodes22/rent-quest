d0e93a5b6f20413206001650f893ddb83beff03f
Ali Ahmed <alitine94@gmail.com>
2024-06-03 01:39:09 -0400

    search component, message model

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,44 +1,78 @@
-"use client"
+"use client";
 
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-
-
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
+import SearchForm from "@/components/SearchForm";
 
 const SearchResultsPage = () => {
-    const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
 
-    const[properties, setProperties] = useState([]);
-    const[loading, setLoading] = useState(true);
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const location = searchParams.get("location");
-    const propertyType = searchParams.get("propertyType");
+  const location = searchParams.get("location");
+  const propertyType = searchParams.get("propertyType");
 
-    useEffect(() => {
-        const fetchProperties = async() => {
-            try {
-              
-                const res = await fetch (`/api/properties/search?location=${location}&propertyType=${propertyType}`);
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties/search?location=${location}&propertyType=${propertyType}`
+        );
 
-                if(res.status === 200) {
-                    const data = await res.json();
-                    setProperties(data);
-                } else {
-                    setProperties([]);
-                }
-            } catch (error) {
-                console.log(error)
-            } finally {
-                setLoading(false);
-            }
+        if (res.status === 200) {
+          const data = await res.json();
+          setProperties(data);
+        } else {
+          setProperties([]);
         }
-        fetchProperties();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProperties();
+  }, [location, propertyType]);
 
-    }, [location, propertyType])
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex-col items-start sm:px-6 lg:px-8">
+          <SearchForm />
+        </div>
+      </section>
 
-    return (
-
-    )
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto">
+            <Link
+              href="/properties"
+              className="flex items-center text-blue-500 hover:underline mb-3"
+            >
+              <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back to listings
+            </Link>
+            <h1 className="text-2xl mb-4">Search Results</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.length === 0 ? (
+                <p>No properties found</p>
+              ) : (
+                properties.map((property) => (
+                  <PropertyCard property={property} key={property._id} />
+                ))
+              )}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default SearchResultsPage;
